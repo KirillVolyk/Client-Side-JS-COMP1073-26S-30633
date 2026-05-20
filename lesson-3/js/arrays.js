3457f63c42f8443ffa6ac42ad5e7a450bbb1d707
Kirill Volyk <200638948@student.georgianc.on.ca>
2026-05-20 10:09:11 -0400

    finished arrays file, moving to receipt

--- lesson-3/js/arrays.js
+++ lesson-3/js/arrays.js
@@ -35,27 +35,41 @@ for (let i = 0; i < arrayLength; i ++){
 
 /* STEP 4: Convert a string to an array
 If there is a common character that can act as a delimiter in a string, we can use this character to create an array */
+let orig6 = "Toronto Maple Leafs, Chicago Blackhawks, Detroit Red Wings, Boston Bruins, Montreal Canadiens, New York Rangers";
+let orig6Array = orig6.split(", ");
+console.log(orig6Array);
 
 // Output one of the array items
+console.log(orig6Array[2]);
 
 // Output the last element of the array
+console.log(orig6Array.at(-1))
 
 /* STEP 5: Convert an array back to a string
 Use join() and toString() - note that join() allows you to choose and insert a delimiter, while toString() does not */
+let orig6String = orig6Array.join(", ");
+console.log(orig6String);
 
 /* STEP 6: Adding and removing items from an array
 Without the ability to edit the contents of an array, this type of variable would have limited use - but adding and removing array items is pretty straightforward */
 
-// Adding one or more items to an array with push()
+// Adding one or more items to an array at the end/append with push()
+let numItems = orig6Array.push("Buffalo Sabres", "New York Islanders");
 
 // If you would like to capture how many elements are in the array after you have edited it, then…
 
-// Removing an item from an array with pop()
+// Removing an item from an array at the end with pop()
+let itemRemoved = orig6Array.pop();
 
 // pop() returns the item that was removed, rather than the length of the updated array, so…
 
 // To do the same thing, that is, to add and remove an item from the beginning of the array, use shift() and unshift()
+// shift to the right (-1 to index)
+// unshift to the left (+1 to index) and what youre adding to the end
+let removedItem = orig6Array.shift();
+numItems = orig6Array.unshift("Winnipeg Jets", "Quebec Nordiques");
 
 // We can also modify the array contents by deleting or substituting elements, or inserting one or more elements at a certain place with splice()
+orig6Array.splice(3,3, "Edmonton Oilers");
 
 /* That's it for the basics of working with arrays! With these tools at your disposal, a whole new world of possibilities with JavaScript are at your command */
