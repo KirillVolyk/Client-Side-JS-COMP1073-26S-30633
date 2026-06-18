52a059ebf0d8255dae1733ca58f50244c90862a5
Kirill Volyk <200638948@student.georgianc.on.ca>
2026-06-17 23:14:10 -0400

    made some progress on figuring out a good way for indexing buttons

--- assignment-1/js/script.js
+++ assignment-1/js/script.js
@@ -5,4 +5,21 @@ var part3 = ["a distant planet", "the sun", "a black hole", "an abandoned space 
 var part4 = ["at incredible speed", "while spinning out of control", "with weapons fully charged", "while shaking violently", "with unstoppable momentum"];
 var part5 = ["in deep space", "near the Milky Way", "beyond the asteroid belt", "inside a nebula", "at the edge of the universe"]
 
-// make each button loop through array and replace text
+// make an index tracker. when button is hit an index is incremented (could reset by diving it by the lenght with modulo? Test it)
+
+// indexes
+var index1 = 0;
+var index2 = 0;
+var index3 = 0;
+var index4 = 0;
+var index5 = 0;
+
+// incrementing function
+function indexInc(index)
+{
+    
+}
+
+// button event
+let choiceOne = document.getElementById("btn1")
+choiceOne.addEventListener("click", indexInc())
