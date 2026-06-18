dd32ffb2178c1b9ab4eeebff191eb8d4eded2940
Kirill Volyk <200638948@student.georgianc.on.ca>
2026-06-17 23:09:20 -0400

    startign to make a script(arrays for the choices)

--- assignment-1/js/script.js
+++ assignment-1/js/script.js
@@ -1,1 +1,8 @@
+// make arrays of story possibilities
+var part1 = ["A starship", "The Deathstar", "A deep‑space shuttle", "A goofy alien", "A scorching meteor"];
+var part2 = ["is firing at", "is ramming into", "is accelerating toward", "is closing in on", "is drifting toward"];
+var part3 = ["a distant planet", "the sun", "a black hole", "an abandoned space station", "a galaxy"];
+var part4 = ["at incredible speed", "while spinning out of control", "with weapons fully charged", "while shaking violently", "with unstoppable momentum"];
+var part5 = ["in deep space", "near the Milky Way", "beyond the asteroid belt", "inside a nebula", "at the edge of the universe"]
+
 // make each button loop through array and replace text
