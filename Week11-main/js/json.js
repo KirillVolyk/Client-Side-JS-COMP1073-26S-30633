848974af4f3f3d7ec9cdea1888457f9d3c5f9065
Kirill Volyk <200638948@student.georgianc.on.ca>
2026-07-22 20:12:47 -0400

    finished lesson 11, and added 3 flavours (1 in class, 2 for lab)

--- Week11-main/js/json.js
+++ Week11-main/js/json.js
@@ -8,12 +8,11 @@ async function populate(){
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4a: Create i-scream.json file with companyName, headOffice, established, active, topFlavors(name, calories, type, ingredients, image) */
     // STEP 4b: Store the URL of a JSON file in a variable */
-    let url = "https://kirillvolyk.github.io/Client-Side-JS-COMP1073-26S-30633/Week11-main/i-scream.json";
+    const url = "https://kirillvolyk.github.io/Client-Side-JS-COMP1073-26S-30633/Week11-main/i-scream.json";
 
     // STEP 5: Use the new URL to create a new request object
     const request = new Request(url);
     
-
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
     const response = await fetch(request);
     // Check if it works
@@ -22,18 +21,17 @@ async function populate(){
     // STEP 7: Capture the returned Response object and covert to a JSON object using json()
     const responseJson = await response.json();
     
-
     // STEP 8: Output the iScream JSON object to the console
     // Check if it works
     console.log(responseJson);
 
     // STEP 9a: Invoke the populateHeader function here, then build it below
-
+    populateHeader(responseJson);
 
     // STEP 10a: Invoke the showTopFlavors function here, then build it below
+    showTopFlavors(responseJson);
 
-
-};
+}
 
 
 
@@ -41,28 +39,67 @@ async function populate(){
 populate();
 
 /* STEP 9b: Build out the populateHeader() function */
+function populateHeader(json){
 
-// Create the H1 element
+    // Create the H1 element
+    let h1 = document.createElement("h1");
+    let para = document.createElement("p");
 
-// Grab the company name from the JSON object and use it for the text node
+    // Grab the company name from the JSON object and use it for the text node
+    h1.textContent = json.companyName;
+    para.textContent = `Heaad Office: ${json.headOffice}, est. ${json.established}, Status: ${json.active ? "Active" : "Inactive"}`
+    // Inject the complete H1 element into the DOM, inside the HEADER
+    header.appendChild(h1);
+    header.appendChild(para);
 
-// Inject the complete H1 element into the DOM, inside the HEADER
+}
 
 /* STEP 10b: Assemble the showTopFlavors() function */
+function showTopFlavors(json){
 
-// STEP 10c: Bind the JSON topFlavors object to a var
+    // STEP 10c: Bind the JSON topFlavors object to a var
+    let topFlavors = json.topFlavors;
 
-// STEP 10d: Loop through the topFlavors object
+    // STEP 10d: Loop through the topFlavors object
+    for (let i = 0; i < topFlavors.length; i++) {
+        console.log(topFlavors[i])
 
-// STEP 10e: build HTML elements for the content: article, h2, image, p1, p2, list
+        // STEP 10e: build HTML elements for the content: article, h2, image, p1, p2, list
+        let article = document.createElement("article");
+        let h2 = document.createElement("h2");
+        let image = document.createElement("img");
+        let p1 = document.createElement("p");
+        let p2 = document.createElement("p");
+        let list = document.createElement("ul");
+        
+        // STEP 10f: Set the textContent property for each of the above elements (except the UL), based on the JSON content
+        h2.textContent = "Name: " + topFlavors[i].name;
+        p1.textContent = "Calories: " + topFlavors[i].calories;
+        p2.textContent = "Type: " + topFlavors[i].type;
+        image.setAttribute("src", topFlavors[i].image);
 
-// STEP 10f: Set the textContent property for each of the above elements (except the UL), based on the JSON content
+        // STEP 10g: Build a loop for the ingredients array in the JSON
+        let ingredients = topFlavors[i].ingredients
 
-// STEP 10g: Build a loop for the ingredients array in the JSON
+        for (let j = 0; j < ingredients.length; j++){
+            let listItem = document.createElement("li");
+            listItem.textContent = ingredients[j];
+            list.appendChild(listItem);
+        }
 
-// STEP 10i: Append each complete ARTICLE element to the SECTION element
+        // STEP 10i: Append each complete ARTICLE element to the SECTION element
+        article.appendChild(h2);
+        article.appendChild(p1);
+        article.appendChild(p2);
+        article.appendChild(list);
+        article.appendChild(image);
 
-// STEP 11: Add a 3rd flavour of ice cream to the local JSON file, making use of the /images/strawberry-sprinkle.svg image
+        section.appendChild(article);
+    }
+
+    // STEP 11: Add a 3rd flavour of ice cream to the local JSON file, making use of the /images/strawberry-sprinkle.svg image
+
+}
 
 // This page inspired by and adapted from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON
 
