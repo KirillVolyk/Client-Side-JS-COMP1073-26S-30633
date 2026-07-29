e439c1365540f9b7dcb5e4e7102ff723fca4c308
Kirill Volyk <200638948@student.georgianc.on.ca>
2026-07-28 23:41:13 -0400

    Starting on the js

---
+++ assignment-2/js/pizzajs.js
@@ -0,0 +1,14 @@
+// Variable for output pizza details
+const output = document.querySelector("#order-output");
+
+// Find and replace Student info
+const studentName = document.querySelector("#studentName");
+const studentID = document.querySelector("#studentID")
+
+studentName.textContent = "Kirill Volyk(JS)";
+studentID.textContent = "200638948(JS)";
+
+// Capture the variable of the form
+
+
+// Validate that the form is not empty
