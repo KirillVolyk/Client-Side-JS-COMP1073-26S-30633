f6d91773e67c97360e2d9ca2f6bd476f42bf2c00
Kirill Volyk <200638948@student.georgianc.on.ca>
2026-08-06 00:35:10 -0400

    finished lesson 12 and lab

--- Week12-main/js/orientation.js
+++ Week12-main/js/orientation.js
@@ -25,21 +25,37 @@ const statusMsg = document.querySelector("#status");
 /* Script Logic
 -------------------------------------------------- */
 // STEP 2a: Check support for the DeviceOrientationEvent
+if (!window.DeviceOrientationEvent) {
 
-// STEP 2b: Do something helpful if there is no support for the DeviceOrientationEvent
+    // STEP 2b: Do something helpful if there is no support for the DeviceOrientationEvent
+    statusMsg.textContent = "DeviceOrientationEvent is not supported on this device.";
 
-// STEP 3a: Build an event listener for the 'deviceorientation' event, and build out an anonymous callback function, passing in the event itself
+} else {
 
-// STEP 3b: Capture the alpha value (rotation) and set the textContent for the <output> element
+    // STEP 3a: Build an event listener for the 'deviceorientation' event, and build out an anonymous callback function, passing in the event itself
+    window.addEventListener('deviceorientation', function(event) {
+        console.log(event);
 
-// STEP 3c: Use the same value to update the position of the slider
+        // STEP 3b: Capture the alpha value (rotation) and set the textContent for the <output> element
+        alphaValue.textContent = Math.round(event.alpha) + "°";
 
-// STEP 3d: Capture the beta value (tilt toward/away) and set the textContent for the <output> element
+        // STEP 3c: Use the same value to update the position of the slider
+        alphaSlider.value = Math.round(event.alpha);
 
-// STEP 3e: Use the same value to update the position of the slider
+        // STEP 3d: Capture the beta value (tilt toward/away) and set the textContent for the <output> element
+        betaValue.textContent = Math.round(event.beta) + "°";
 
-// STEP 3f: Capture the gamma value (tilt left/right) and set the textContent for the <output> element
+        // STEP 3e: Use the same value to update the position of the slider
+        betaSlider.value = Math.round(event.beta);
 
-// STEP 3g: Use the same value to update the position of the slider
+        // STEP 3f: Capture the gamma value (tilt left/right) and set the textContent for the <output> element
+        gammaValue.textContent = Math.round(event.gamma) + "°";
+
+        // STEP 3g: Use the same value to update the position of the slider
+        gammaSlider.value = Math.round(event.gamma);
+        
+    });
+
+}
 
 /* Learn more at https://developer.mozilla.org/en-US/docs/Web/API/DeviceOrientationEvent */
